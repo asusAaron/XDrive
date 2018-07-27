1092b8bffdaf17ac3171ba210167b2479dfd9156
asusAaron <1316339697@qq.com>
2018-07-27 13:51:41 +0800

    sql

--- src/main/resources/tables.sql
+++ src/main/resources/tables.sql
@@ -1,20 +1,22 @@
 CREATE TABLE user  (
-  `u_id` int(0) NOT NULL AUTO_INCREMENT,
   `u_account` varchar(30) NOT NULL,
   `u_name` varchar(18) NOT NULL,
   `u_password` varchar(20) NOT NULL,
   `u_date` date NOT NULL,
   `u_limit` int(1) NOT NULL,
-  PRIMARY KEY (`u_id`)
+  PRIMARY KEY (`u_account`)
 );
 
 CREATE TABLE file  (
   `f_id` int(0) NOT NULL AUTO_INCREMENT,
   `f_name` varchar(20) NOT NULL,
-  `f_limit` varchar(1) NOT NULL,
+  `f_limit` int(1) NOT NULL,
   `f_uploadtime` datetime(0) NOT NULL,
-  `f_userid` int(0) NOT NULL,
+  `f_parent` varchar(20),
+  `f_type` int(1) NOT NULL,
+  `f_state` int(1) NOT NULL,
+  `f_user` varchar(30) NOT NULL,
   PRIMARY KEY (`f_id`),
-  CONSTRAINT `FK_USERID` FOREIGN KEY (`f_userid`) REFERENCES `xdrive`.`user` (`u_id`)
+  CONSTRAINT `FK_USER` FOREIGN KEY (`f_user`) REFERENCES `xdrive`.`user` (`u_account`)
 );
 
